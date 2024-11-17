1b4c3471138f0f6f791a9de6315cdaa358ca71c9
Moment49 <ibenachoelvis49@gmail.com>
2024-11-17 02:54:37 +0100

    added the register page with the view on django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view
+from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path('list-books/', list_books, name="list-books"),
     path('library-books/<int:pk>', LibraryDetailView.as_view(), name="library-books"),
-    path('register/', register_view, name="register"),
+    path('register/', views.register, name="register"),
 ]
