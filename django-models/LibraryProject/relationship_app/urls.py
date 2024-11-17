2b55a97e471c8be3942cfc8cbdc412cd1bd204c7
Moment49 <ibenachoelvis49@gmail.com>
2024-11-17 18:29:38 +0100

    worked on the authentication for user

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('list-books/', list_books, name="list-books"),
     path('library-books/<int:pk>', LibraryDetailView.as_view(), name="library-books"),
     path('register/', views.register, name="register"),
-    path('login/', LoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(), name="logout"),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
+    path('logout/', LogoutView.as_view('registration/logout.html'), name="logout"),
     path('', home, name="home"),
 ]
