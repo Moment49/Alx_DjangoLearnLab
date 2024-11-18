726327d05871b085328cb40651bc8c67e36fbe84
Moment49 <ibenachoelvis49@gmail.com>
2024-11-18 14:40:08 +0100

    worked on the memeber, admin and librarian views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView, home
+from .views import list_books, LibraryDetailView, LoginView, LogoutView, home, admin_view, member_view, librarian_view
 from . import views
 
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name="logout"),
     path('', home, name="home"),
+    path('admin_view/', admin_view, name='admin_view'),
+    path('member_view/', member_view, name='member_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,14 @@
 from typing import Any
 from django.shortcuts import render, redirect
-from .models import Library, Book
+from .models import Library, Book, UserProfile
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .forms import RegiesterForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 # Function based View
@@ -26,8 +28,6 @@ class LibraryDetailView(DetailView):
     # This performs the query as an attribute of the class (can be overidden)
     # use queryset when you want to use the ListView
     # queryset = Library.objects.get(name="tech library")
-
-     
     def get_context_data(self, **kwargs: Any):
         """This is to add more data context to the View"""
         context_data = super().get_context_data(**kwargs)
@@ -52,7 +52,36 @@ class LoginView(LoginView):
 class LogoutView(LogoutView):
     template_name = 'registration/logout.html'
 
+
 def home(request):
     return render(request, 'relationship_app/home.html')
+
+
+def is_admin(user):
+    user = UserProfile.objects.get(user=user)
+    if user.roles != "Admin":
+        return False
+    return user
+
+def is_librarian(user):
+    user = UserProfile.objects.get(user=user)
+    if user.roles == "Librarian":
+        return user
+def is_member(user):
+    user = UserProfile.objects.get(user=user)
+    if user.roles == "Member":
+        return user
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
     
