4231313e4a51d3662be111642a7ba666274d0cb3
Moment49 <ibenachoelvis49@gmail.com>
2024-11-26 11:31:24 +0100

    worked on task 0 for week 11

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -6,32 +6,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
 # Register your models here.
-# class MyUserChangeForm(UserChangeForm):
-#     class Meta:
-#         model = CustomUser
-#         fields = ["username", "date_of_birth", "password", "profile_photo"]
-
-# class CustomUserAdmin(BaseUserAdmin):
-#     form = MyUserChangeForm
-#     list_display = ["date_of_birth", "profile_photo", "username"]
-
-#     fieldsets = [
-#         (None, {"fields": ["username", "password"]}),
-#         ("Personal info", {"fields": ["date_of_birth", "profile_photo"]}),
-        
-#     ]
-
-#     # Uses this field set when creating the user
-#     add_fieldsets = [
-#         (
-#             None,
-#             {
-#                 "classes": ["wide"],
-#                 "fields": ["username", "date_of_birth", "password1", "password2", "profile_photo"],
-#             },
-#         ),
-#     ]
-
 admin.site.register(UserProfile)
 admin.site.register(Book)
 
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,49 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.conf import settings
 
 
 
 # Create your models here.
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, username, date_of_birth, password=None):
-#         if not username:
-#             raise ValueError("username is required")
-#         if not password:
-#             raise ValueError("password is required")
-#         if not date_of_birth:
-#             raise ValueError("Date of birth is required")
-#         # create the user obj
-#         user = self.model(date_of_birth=date_of_birth, username=username)
-
-#         user.set_password(password)
-#         user.save(using=self._db)
-
-#         return user
-    
-#     def create_superuser(self, username, date_of_birth, password=None):
-#         user = self.create_user(username=username,date_of_birth=date_of_birth, password=password)
-#         user.is_staff = True
-#         user.is_admin = True
-#         user.is_active = True
-#         user.is_superuser = True
-
-#         user.save(using=self._db)
-
-#         return user
-            
-
-
-# class CustomUser(AbstractUser):
-#     profile_photo = models.ImageField(upload_to='uploads/', blank=True, null=True)
-#     date_of_birth = models.DateField(null=True, blank=True)
-
-#     objects = CustomUserManager()
-#     REQUIRED_FIELDS = ['date_of_birth']
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
