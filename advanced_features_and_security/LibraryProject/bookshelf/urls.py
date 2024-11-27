96ccb42bed9b917dbbcbb4c3302852dbd0e69ced
Moment49 <ibenachoelvis49@gmail.com>
2024-11-28 00:53:05 +0100

    permissions assigned to groups

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('edit/<int:id>', views.edit, name='edit'),
     path('delete/<int:id>', views.delete, name='delete'),
-    path('view/', views.view, name='view')
+    path('view/', views.view, name='view'),
+    path('book_list/', views.book_list, name='book_list'),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 
 # Create your views here.
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/books_list.html", {"booka": books})
+
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create(request):
     return render(request, "bookshelf/create.html")
