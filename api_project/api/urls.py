bfcd621dddd68df7e22674d54936ca0bd2d08ac2
Moment49 <ibenachoelvis49@gmail.com>
2024-12-07 16:04:10 +0100

    auth api permission compeleted

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register(r"book_all", BookViewSet, basename="book_all")
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('api-token-auth/', views.obtain_auth_token),
     path('', include(router.urls))
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,12 +4,17 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # This is the authentication and permission that was set for the view
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
 
 class BookViewSet(viewsets.ModelViewSet):
