4182846d184c93a81b0dd079c8d642de3b323306
Moment49 <ibenachoelvis49@gmail.com>
2024-12-13 17:09:18 +0100

    updated now

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('edit_profile/<str:user>', views.edit_profile, name="edit_profile"),
     path('posts/', views.PostListView.as_view(), name="all_posts"),
     path('posts/<int:pk>', views.PostDetailView.as_view(), name="detail_post"),
-    path('posts/new/', views.PostCreateView.as_view(), name="create_post"),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name="update_post"),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name="delete_post"),
+    path('post/new/', views.PostCreateView.as_view(), name="create_post"),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name="update_post"),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name="delete_post"),
     path('dashboard/', views.dashboard, name="dashboard"),
     
 ]
