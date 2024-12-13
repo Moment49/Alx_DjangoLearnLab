c2c9f7a07a9020adb1a919dd5c8093e8caea1892
Moment49 <ibenachoelvis49@gmail.com>
2024-12-13 14:07:59 +0100

    worked on the blog post latest NOW

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth import logout
 from .models import UserProfile, User, Post
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.views.generic.detail import DetailView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
