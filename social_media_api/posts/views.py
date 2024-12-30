1195732eca67373228a79e709a7ae6e8ac6fc20e
Moment49 <ibenachoelvis49@gmail.com>
2024-12-30 19:13:09 +0100

    working on task 1 for week 15

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,13 +2,12 @@ from posts.serializers import PostSerializer, CommentSerializer
 from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from posts.models import Post, Comment
 
 # Create your views here.
 
-class PostViewset(ModelViewSet):
+class PostViewset(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    
