20c57fe20618599507a61ce9b7c6c2fb493e0c09
Moment49 <ibenachoelvis49@gmail.com>
2024-12-30 18:41:43 +0100

    working on task 1 for week 15

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -7,13 +7,13 @@ User = get_user_model()
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=200)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    content = models.TextField(max_length=250)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
