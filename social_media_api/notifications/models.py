106853d463d54fd5f11677ba9e1049f70eb57b79
Moment49 <ibenachoelvis49@gmail.com>
2025-01-05 20:22:27 +0100

    worked on the feed and follow and unfollow features

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -6,12 +6,12 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 CustomUser = get_user_model()
 
 # Create your models here.
-class Notification(models.Mode):
+class Notification(models.Model):
     recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="notifications")
-    actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="notifications")
-    verb = models.CharField()
-    object_id = models.PositiveIntegerField()
-    target = GenericForeignKey("object_id")
-    timestamp = models.DateTimeField(auto_now=True)
+    # actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="notifications")
+    # verb = models.CharField()
+    # object_id = models.PositiveIntegerField()
+    # target = GenericForeignKey("object_id")
+    # timestamp = models.DateTimeField(auto_now=True)
 
   
